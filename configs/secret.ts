a91e74339028d3440fabdd31d11f8a787464569b
0x404500 <112140184+0x404500@users.noreply.github.com>
2022-12-16 08:39:08 +0700

    Create secret.ts

---
+++ configs/secret.ts
@@ -0,0 +1,30 @@
+export const appEncryptKey = '';
+export const pinEncryptKey = '';
+export const OpenseaApiKey = '';
+export const QuixoticApiKey = '';
+
+export const etherscanApiKeys: { [chainId: number]: string[] } = {
+  1: [],
+  10: [], // optimism
+  42161: [], // arbitrum
+  137: [],
+  56: [],
+  1313161554: [],
+
+  250: [], // fantom
+  43114: [], // avalanche
+  1285: [], // Moonriver
+  1284: [], // Moonbeam
+  128: [],
+};
+
+export const alchemyApiKeys: string[] = [];
+
+export const DeBankApiKey = '';
+
+export const GoogleSignInConfigs = {
+  webClientId: '',
+};
+
+export const swapFeePercent = 0;
+export const swapFeeReferrer = '';
